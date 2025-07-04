d5aceaf62da508756f47812a230121188cee7d47
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2025-07-04 19:24:50 -0300

    update
    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import * as express from 'express';
 import * as path from 'path';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: '*', // Ajuste conforme necessário para definir as origens permitidas
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
@@ -16,10 +17,30 @@ async function bootstrap() {
 
   app.enableCors(corsOptions);
 
-   // Configuração do diretório estático
-  const publicDir = path.join(__dirname, '..', '..', 'src', 'public'); // Ajuste o caminho para refletir a estrutura após a compilação
+  const publicDir = path.join(__dirname, '..', '..', 'src', 'public');
 
-  app.use('/image', express.static(publicDir));
+  app.use('/image', (req, res, next) => {
+    const filePath = path.join(publicDir, req.path);
+    console.log('aqui foi chamanda', filePath)
+    
+
+    if (!fs.existsSync(filePath)) {
+      console.log('aqui o FS', filePath)
+      return res.status(404).send('Arquivo não encontrado');
+    }
+
+    if (filePath.toLowerCase().endsWith('.mp4')) {
+      console.log('mp4 aqui')
+      res.setHeader('Content-Type', 'video/mp4');
+      res.setHeader('Content-Disposition', 'inline');
+
+      const stream = fs.createReadStream(filePath);
+      return stream.pipe(res);
+    }
+
+    // Se não for .mp4, delega para o middleware padrão
+    express.static(publicDir)(req, res, next);
+  });
 
   await app.listen(3333);
   console.log(`Aplicação rodando em: http://localhost:3333`);
