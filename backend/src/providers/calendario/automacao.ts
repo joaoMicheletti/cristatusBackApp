df9eb19bd02c596081505afa5d7c5a579978e6aa
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2025-08-14 20:38:30 -0300

    update
    update

--- backend/src/providers/calendario/automacao.ts
+++ backend/src/providers/calendario/automacao.ts
@@ -279,7 +279,8 @@ export class Automacao {
                         });
                         this.logger.debug('acompanhamento do status do container', statusRes.data);
 
-                        if (statusRes.data.status === 'Finished: Media has been uploaded and it is ready to be published.'){
+                        if (statusRes.data.status_code === 'FINISHED'){
+                            
                             this.logger.debug('Finished')
                             Verification += 1;
                             break;
